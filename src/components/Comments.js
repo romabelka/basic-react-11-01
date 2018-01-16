9169ab0706b6e34ab8facd55436434d361c0d0ee
Ilya Lisitsin <ilyas.sporichev@gmail.com>
2018-01-16 16:06:10 +0300

    trash removed

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -1,13 +1,9 @@
 import React, { Component } from 'react'
 
 class Comments extends Component {
-	// construnctor(props) {
-	// 	super(props)
-	// }
-
 
 	render() {
-		console.log(123123123, this.props)
+
 		const commentElements = this.props.comments.map((comment, index) => <ul>
 			<li key={comment.id}>
 				<div>{comment.text}</div>
