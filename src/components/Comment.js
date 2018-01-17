e897138224e2698f620b7a20048d8d62fea18dda
Айрат Жаншуаков <dakanca@gmail.com>
2018-01-17 22:48:35 +0600

    Improve commentary components

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,19 +1,12 @@
 import React, {Component} from 'react'
 
-class Comment extends Component {
-  constructor (props) {
-    super(props)
-  }
-
-  render () {
-    const {comment} = this.props
-    return (
-      <React.Fragment>
-        <p>{comment.text}</p>
-        Comment by: <strong>{comment.user}</strong>
-      </React.Fragment>
-    )
-  }
+function Comment ({comment}) {
+  return (
+    <li>
+      <p>{comment.text}</p>
+      Comment by: <strong>{comment.user}</strong>
+    </li>
+  )
 }
 
 export default Comment
--- src/components/CommentsList.js
+++ src/components/CommentsList.js
@@ -11,20 +11,27 @@ class CommentsList extends Component {
   }
 
   render () {
+    const pluralization = `comment${this.props.comments.length > 1 ? 's' : ''}`
+
     const comments = this.props.comments.map(comment => {
-      return <li key={comment.id}><Comment comment = {comment}/></li>
+      return <Comment key={comment.id} comment = {comment}/>
     })
+
     const commentsList = this.state.isOpen ? comments : ''
+
     return (
       <div>
         <button onClick={this.showComments}>
-          {this.isOpen ? 'Hide comments' : 'Show comments'}
+          {this.isOpen ? `Hide ${pluralization}` : `Show ${pluralization}`}
         </button>
         <ul>{commentsList}</ul>
       </div>
     )
   }
 
+  /**
+   * Comment visibility switcher
+   */
   showComments = () => {
     this.setState((state) => ({
       isOpen: !state.isOpen
