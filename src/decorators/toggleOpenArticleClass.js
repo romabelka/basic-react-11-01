f9bfc92e743bd81913cac8431d603067785739ef
Eli Kry <eljobemail@gmail.com>
2018-01-22 00:11:24 +0300

    1234

--- src/decorators/toggleOpenArticleClass.js
+++ src/decorators/toggleOpenArticleClass.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react'
 
-export default class DecoratorClass extends Component {
+export default class toggleOpenClass extends Component {
     state = {
         openArticleId: null
     }
 
-    toggleOpenArticle = (openArticleId) => {
+    toggleOpenArticle = (openArticleId) => () => {
         this.setState(state => {
             return {
                 openArticleId: openArticleId === state.openArticleId ? null : openArticleId
