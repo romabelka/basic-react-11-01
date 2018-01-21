bf145b47326566be87fc5fabbf0a8f81d2f8e3a1
DrShpongle <evgeniy.nagalskiy@gmail.com>
2018-01-21 18:26:10 +0200

    ffix shouldComponentUpdate for PureComponent

--- src/components/Article.js
+++ src/components/Article.js
@@ -3,49 +3,49 @@ import PropTypes from 'prop-types'
 import CommentList from './CommentList'
 
 class Article extends PureComponent {
-    static propTypes = {
-/*
-        defaultOpen: PropTypes.bool, //if Article is open by default
-*/
-        article: PropTypes.shape({
+  static propTypes = {
+    /*
+            defaultOpen: PropTypes.bool, //if Article is open by default
+    */
+    article: PropTypes.shape({
 //            id: PropTypes.string.isRequired,
-            title: PropTypes.string.isRequired,
-            text: PropTypes.string,
-            comments: PropTypes.array
-        }).isRequired
+      title: PropTypes.string.isRequired,
+      text: PropTypes.string,
+      comments: PropTypes.array
+    }).isRequired
+  }
+  
+  constructor(props) {
+    super(props)
+    
+    this.state = {
+      foo: 'bar'
     }
-
-    constructor(props) {
-        super(props)
-
-        this.state = {
-            foo: 'bar'
-        }
-    }
-
-    render() {
-        console.log('---', 'rerendering')
-        const {article, isOpen, onButtonClick} = this.props
-        const body = isOpen && (
-            <div>
-                <section>{article.text}</section>
-                <CommentList comments = {article.comments} />
-            </div>
-        )
-        return (
-            <div>
-                <h2>
-                    {article.title}
-                    <button onClick={(ev) => onButtonClick(ev, article)}>
-                        {isOpen ? 'close' : 'open'}
-                    </button>
-                </h2>
-                {body}
-                <h3>creation date: {(new Date(article.date)).toDateString()}</h3>
-            </div>
-        )
-    }
-
+  }
+  
+  render() {
+    console.log('---', 'rerendering')
+    const {article, isOpen, onButtonClick} = this.props
+    const body = isOpen && (
+      <div>
+        <section>{article.text}</section>
+        <CommentList comments={article.comments}/>
+      </div>
+    )
+    return (
+      <div>
+        <h2>
+          {article.title}
+          <button onClick={(ev) => onButtonClick(article.id)}>
+            {isOpen ? 'close' : 'open'}
+          </button>
+        </h2>
+        {body}
+        <h3>creation date: {(new Date(article.date)).toDateString()}</h3>
+      </div>
+    )
+  }
+  
 }
 
 
