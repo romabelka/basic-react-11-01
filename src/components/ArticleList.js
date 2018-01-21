2fb8eea4021c1a89df665af010304bb1045bb9f6
omiceron <omiceron@gmail.com>
2018-01-22 00:10:04 +0500

    - Close button fixed - Article rendering optimised

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -17,9 +17,10 @@ class ArticleList extends Component {
 
         const articleElements = this.props.articles.map((article, index) => <li key = {article.id}>
             <Article article = {article}
-                     defaultOpen = {index === 0}
+                     //defaultOpen = {index === 0}
                      isOpen = {article.id === this.state.openArticleId}
-                     onButtonClick = {this.toggleOpenArticle(article.id)}
+
+                     onButtonClick = {this.toggleOpenArticle}
             />
         </li>)
         return (
@@ -29,7 +30,7 @@ class ArticleList extends Component {
         )
     }
 
-    toggleOpenArticle = (openArticleId) => () => this.setState({ openArticleId })
+    toggleOpenArticle = (openArticleId) => this.setState({openArticleId: this.state.openArticleId !== openArticleId ? openArticleId : null})
 }
 
 export default ArticleList
