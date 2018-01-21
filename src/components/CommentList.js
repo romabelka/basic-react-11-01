f41d86aa90a66ba781185be033597e83398688a0
AbilityG <tursumbekov@mail.ru>
2018-01-22 00:43:40 +0600

    Hometask_02

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react'
+import PropTypes from 'prop-types'
 import Comment from './Comment'
 import toggleOpen from '../decorators/toggleOpen'
 
@@ -32,6 +33,12 @@ function getBody(props) {
 
 CommentList.defaultProps = {
     comments: []
+}
+
+CommentList.propTypes = {
+    isOpen: PropTypes.bool,
+    toggleOpen: PropTypes.func,
+    comments: PropTypes.array
 }
 
 /*
