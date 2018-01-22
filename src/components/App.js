3ff1c56eafabecb5e49af6891aaa0d4df53bb6a0
Roman Iakobchuk <kaboom.ua@gmail.com>
2018-01-22 21:26:19 +0200

    add user form

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import ArticleList from './ArticleList'
 import ArticlesChart from './ArticlesChart'
+import UserForm from './UserForm'
 import articles from '../fixtures'
 
 function App() {
     return (
         <div>
             <h1>App name</h1>
+            <UserForm />
             <ArticleList articles = {articles} />
             <ArticlesChart articles = {articles} />
         </div>
