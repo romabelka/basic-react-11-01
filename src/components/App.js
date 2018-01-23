c30ecab0bbf660604dd66711c163161907977d4f
Denis1990 <den.bogdanov4@gmail.com>
2018-01-23 23:00:01 +0300

    Add form and validate

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react'
 import ArticleList from './ArticleList'
 import ArticlesChart from './ArticlesChart'
-import UserForm from './UserForm'
 import Select from 'react-select'
 import articles from '../fixtures'
 
@@ -22,7 +21,6 @@ class App extends Component {
         return (
             <div>
                 <h1>App name</h1>
-                <UserForm />
                 <Select options = {options} value = {this.state.selected} onChange = {this.handleChange} multi />
                 <ArticleList articles = {articles} defaultOpenItemId = {articles[0].id}/>
                 <ArticlesChart articles = {articles} />
--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -1,48 +1,16 @@
 import React, {Component} from 'react'
 import PropTypes from 'prop-types'
 import Comment from './Comment'
+import Input from './Input'
 import toggleOpen from '../decorators/toggleOpen'
 
-function CommentList(props) {
-    const {isOpen, toggleOpen} = props
-    const text = isOpen ? 'hide comments' : 'show comments'
-    return (
-        <div>
-            <button onClick={toggleOpen}>{text}</button>
-            {getBody(props)}
-        </div>
-    )
-}
-
-function getBody(props) {
-    const {comments, isOpen} = props
-    if (!isOpen) return null
-
-    const body = comments.length ? (
-        <ul>
-            {comments.map(comment => <li key = {comment.id}><Comment comment = {comment} /></li>)}
-        </ul>
-    ) : <h3>No comments yet</h3>
-
-    return (
-        <div>
-            {body}
-        </div>
-    )
-}
-
-CommentList.propTypes = {
-    comments: PropTypes.array.isRequired,
-    isOpen: PropTypes.bool,
-    toggleOpen: PropTypes.func
-}
-
-CommentList.defaultProps = {
-    comments: []
-}
-
-/*
 class CommentList extends Component {
+    static propTypes = {
+        comments: PropTypes.array.isRequired,
+        isOpen: PropTypes.bool,
+        toggleOpen: PropTypes.func
+    }
+
     static defaultProps = {
         comments: []
     }
@@ -53,30 +21,29 @@ class CommentList extends Component {
         return (
             <div>
                 <button onClick={toggleOpen}>{text}</button>
-                {this.getBody()}
+                {this.getBody(this.props)}
             </div>
         )
     }
 
-    getBody() {
-        const {comments, isOpen} = this.props
+    getBody(props) {
+        const {comments, isOpen} = props
         if (!isOpen) return null
-
+    
         const body = comments.length ? (
             <ul>
                 {comments.map(comment => <li key = {comment.id}><Comment comment = {comment} /></li>)}
             </ul>
         ) : <h3>No comments yet</h3>
-
+    
         return (
             <div>
+                <Input label = 'Your name' />
+                <Input label = 'Your text' fieldType= 'textarea' />
                 {body}
             </div>
         )
     }
-
 }
-*/
-
 
 export default toggleOpen(CommentList)
