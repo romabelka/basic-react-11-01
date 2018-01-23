fa3d1d591c4587fd6466ae8b286e937e7a2cbc52
Denis1990 <den.bogdanov4@gmail.com>
2018-01-23 23:09:40 +0300

    Add datePicker

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import ArticleList from './ArticleList'
 import ArticlesChart from './ArticlesChart'
 import Select from 'react-select'
 import articles from '../fixtures'
+import RangeDatePicker from './RangeDatePicker'
 
 import 'react-select/dist/react-select.css'
 
@@ -22,6 +23,7 @@ class App extends Component {
             <div>
                 <h1>App name</h1>
                 <Select options = {options} value = {this.state.selected} onChange = {this.handleChange} multi />
+                <RangeDatePicker />
                 <ArticleList articles = {articles} defaultOpenItemId = {articles[0].id}/>
                 <ArticlesChart articles = {articles} />
             </div>
