229db80bb095a92fb790dfaea575a8aaf4d34e19
ed <call007@mail.ru>
2018-01-24 16:44:49 +0300

    Hometask 3

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,11 @@ import ArticleList from './ArticleList'
 import ArticlesChart from './ArticlesChart'
 import UserForm from './UserForm'
 import Select from 'react-select'
+import DayPicker from 'react-day-picker'
 import articles from '../fixtures'
 
 import 'react-select/dist/react-select.css'
+import 'react-day-picker/lib/style.css'
 
 class App extends Component {
     state = {
@@ -24,6 +26,7 @@ class App extends Component {
                 <h1>App name</h1>
                 <UserForm />
                 <Select options = {options} value = {this.state.selected} onChange = {this.handleChange} multi />
+                <DayPicker />
                 <ArticleList articles = {articles} defaultOpenItemId = {articles[0].id}/>
                 <ArticlesChart articles = {articles} />
             </div>
---
+++ src/components/CommentForm.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react'
+
+class CommentForm extends Component {
+    static propTypes = {
+
+    };
+
+    state = {
+        values: {
+            name: '',
+            text: ''
+        },
+        // Очень не нравится хранение флагов на каждое поле, но не смог придумать как сделать по-другому
+        errorInput: {
+            name: false,
+            text: false
+        }
+    }
+
+    render() {
+        return (
+            <fieldset>
+                <legend>Add Your Comment:</legend>
+                <p>
+                    <label>Name:</label>
+                    <br/>
+                    <input
+                        type = "text"
+                        value = {this.state.name}
+                        onChange = {this.handleChangeValidate('name')}
+                        className = {this.state.errorInput.name ? 'error-input' : ''}
+                    />
+                </p>
+                <p>
+                    <label>Text:</label>
+                    <br/>
+                    <textarea
+                        value = {this.state.text}
+                        onChange = {this.handleChangeValidate('text') }
+                        className = {this.state.errorInput.text ? 'error-input' : ''}>
+                    </textarea>
+                </p>
+            </fieldset>
+        )
+    }
+
+    handleChangeValidate = (name) => (ev) => {
+
+        const value = ev.target.value
+        if (value.length > 50) {
+            return;
+        }
+
+        this.setState({
+            values: {
+                [name]: value
+            },
+            errorInput: {
+                [name]: (value.length < 10) ? true : false
+            }
+        })
+
+    }
+
+}
+
+export default CommentForm
