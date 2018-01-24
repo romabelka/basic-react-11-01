c3a541ef8e10987c4fbb863f359ab1ab928bbe18
Ines K <ikuzyuberdina@gmail.com>
2018-01-24 20:36:37 +0300

    Calendar with selecting a range of days

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import ArticlesChart from './ArticlesChart'
 import UserForm from './UserForm'
 import Select from 'react-select'
 import articles from '../fixtures'
+import DayPicker from './DayPicker'
 
 import 'react-select/dist/react-select.css'
 
@@ -22,6 +23,7 @@ class App extends Component {
         return (
             <div>
                 <h1>App name</h1>
+                <DayPicker />
                 <UserForm min = {0} max = {10} />
                 <Select options = {options} value = {this.state.selected} onChange = {this.handleChange} multi />
                 <ArticleList articles = {articles} defaultOpenItemId = {articles[0].id}/>
