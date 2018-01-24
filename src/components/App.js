6782bdebe20ee84b60058fdfe11378377d13cf24
Yevgeny Yakushev <yy@kodeks.ru>
2018-01-24 20:42:10 +0300

    add day picker

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,15 @@
 import React, {Component} from 'react'
 import ArticleList from './ArticleList'
 import ArticlesChart from './ArticlesChart'
-import UserForm from './UserForm'
-import Select from 'react-select'
+import ArticlesFilter from './ArticleList/filter'
 import articles from '../fixtures'
 
-import 'react-select/dist/react-select.css'
-
 class App extends Component {
-    state = {
-        selected: null
-    }
-
-    handleChange = selected => this.setState({ selected })
-
     render() {
-        const options = articles.map(article => ({
-            label: article.title,
-            value: article.id
-        }))
         return (
             <div>
                 <h1>App name</h1>
-                <UserForm />
-                <Select options = {options} value = {this.state.selected} onChange = {this.handleChange} multi />
+                <ArticlesFilter articles = {articles} />
                 <ArticleList articles = {articles} defaultOpenItemId = {articles[0].id}/>
                 <ArticlesChart articles = {articles} />
             </div>
