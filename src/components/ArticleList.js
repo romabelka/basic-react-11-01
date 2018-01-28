f6c4ef9f59ce851361e6e4f98f21064beb8c354e
Viktor Grafskiy <grafviktor@hotmail.com>
2018-01-28 21:27:33 +0300

    HT4

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -6,8 +6,12 @@ import Accordion from './common/Accordion'
 
 class ArticleList extends Accordion {
     render() {
-        const {articles} = this.props
+        let {articles, from, to, keys} = this.props
         if (!articles.length) return <h3>No Articles</h3>
+
+        articles = articlesFilterByDate(articles, from, to);
+        articles = articlesFilterByKeys(articles, keys);
+
         const articleElements = articles.map((article) => <li key={article.id}>
             <Article article={article}
                      isOpen={article.id === this.state.openItemId}
@@ -22,15 +26,39 @@ class ArticleList extends Accordion {
     }
 }
 
+function articlesFilterByDate(articles, from, to) {
+  if(!from || !to) return articles;
+
+  from = new Date(from);
+  to = new Date(to);
+
+  articles = articles.filter(article => {
+      let d = new Date(article.date);
+      return d > from && d <= to;
+  });
+
+  return articles;
+}
+
+function articlesFilterByKeys(articles, keys) {
+    if (keys.length === 0) return articles;
+    return articles.filter(({ id }) => keys.includes(id));
+}
 
 ArticleList.defaultProps = {
     articles: []
 }
 
 ArticleList.propTypes = {
-    articles: PropTypes.array.isRequired
+    articles: PropTypes.array.isRequired,
+    from: PropTypes.instanceOf(Date),
+    to: PropTypes.instanceOf(Date),
+    keys: PropTypes.array.isRequired,
 }
 
 export default connect(state => ({
-    articles: state.articles
+    articles: state.articles,
+    from: state.filter.from,
+    to: state.filter.to,
+    keys: state.filter.keys,
 }))(ArticleList)
